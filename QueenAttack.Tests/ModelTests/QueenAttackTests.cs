4ff31ae3b8787dbabf32e216adb1045c55003cef
John Diggins <rohnfiggins@gmail.com>
2023-02-21 11:23:38 -0800

    4 passed: added QueenY

--- QueenAttack.Tests/ModelTests/QueenAttackTests.cs
+++ QueenAttack.Tests/ModelTests/QueenAttackTests.cs
@@ -10,7 +10,7 @@ namespace QueenAttack.Tests
     [TestMethod] //1
     public void BoardConstructor_CreatesInstanceOfBoard_Board()
     {
-      Board newBoard = new Board(1);
+      Board newBoard = new Board(1, 4);
       Assert.AreEqual(typeof(Board), newBoard.GetType());
     }
     [TestMethod] //2
@@ -18,7 +18,7 @@ namespace QueenAttack.Tests
     {
       //arrange
       int QueenX = 1;
-      Board newBoard = new Board(QueenX);
+      Board newBoard = new Board(QueenX, 4);
       //act
       int result = newBoard.QueenX;
       //assert
@@ -28,17 +28,24 @@ namespace QueenAttack.Tests
     public void SetQueenX_SetsValueOfQueenX_Void()
     {
       //arrange
-      Board newBoard = new Board(1);
+      Board newBoard = new Board(1, 4);
       int newQueenX = 5;
       //act
       newBoard.QueenX = newQueenX;
       //assert  
       Assert.AreEqual(newQueenX, newBoard.QueenX);
     }
-    
+    [TestMethod] //4
+    public void GetQueenY_ReturnsQueenY_Int()
+    {
     //arrange
+    int QueenY = 4;
+    Board newBoard = new Board(1, QueenY);
     //act
+    int result = newBoard.QueenY;
     //assert
+    Assert.AreEqual(QueenY, result);
+    }
     
     //arrange
     //act
