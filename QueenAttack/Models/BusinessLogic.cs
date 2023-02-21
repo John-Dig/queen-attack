6fdfb54ebdea3440ba38180ef9a2ab42f43e2451
John Diggins <rohnfiggins@gmail.com>
2023-02-20 21:23:27 -0800

    wip, between steps 1 and 2, continue business logic

--- QueenAttack/Models/BusinessLogic.cs
+++ QueenAttack/Models/BusinessLogic.cs
@@ -1,1 +1,25 @@
-using System;
+using System.Collections.Generic;
+
+namespace QueenAttack.Models
+{
+  public class Board
+  {
+    // business logic
+
+  }
+}
+
+// code where i stopped ---------------------------copied from Phat
+using System.Collections.Generic;
+
+// create a dictionary with string keys and int values
+Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+
+// add key-value pairs to the dictionary
+myDictionary.Add("x", qx);
+myDictionary.Add("y", qy);
+
+// access the values by key
+int xValue = myDictionary["x"]; // returns qx
+int yValue = myDictionary["y"]; // returns qy
+//-------------------------------------------------
--- QueenAttack/Program.cs
+++ QueenAttack/Program.cs
@@ -1,2 +1,14 @@
 using System;
 using QueenAttack.Models;
+
+namespace QueenAttack
+{
+  class Program
+  {
+    static void Main()
+    {
+      Board testBoard = new Board();
+      Console.WriteLine(testBoard);
+    }
+  }
+}
