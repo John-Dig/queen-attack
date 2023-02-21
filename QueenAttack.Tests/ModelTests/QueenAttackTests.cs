e36fcd12a95c88a61ec9ea90f15f55aaeaa051c1
John Diggins <rohnfiggins@gmail.com>
2023-02-21 10:23:32 -0800

    2 passed

--- QueenAttack.Tests/ModelTests/QueenAttackTests.cs
+++ QueenAttack.Tests/ModelTests/QueenAttackTests.cs
@@ -7,11 +7,25 @@ namespace QueenAttack.Tests
   public class QueenAttack
   {
     
-    [TestMethod]
+    [TestMethod] //1
     public void BoardConstructor_CreatesInstanceOfBoard_Board()
     {
-      Board newBoard = new Board();
+      Board newBoard = new Board(1);
       Assert.AreEqual(typeof(Board), newBoard.GetType());
     }
+    [TestMethod] //2
+    public void GetQueenX_ReturnsQueenX_Int()
+    {
+      //arrange
+      int QueenX = 1;
+      Board newBoard = new Board(QueenX);
+      //act
+      int result = newBoard.QueenX;
+      //assert
+      Assert.AreEqual(QueenX, result);
+    }
+    //arrange
+    //act
+    //assert
   }
 }
