b7e0d8b1be1af096f1c4981be6a55676be9a9518
John-Dig <rohnfiggins@gmail.com>
2023-02-22 15:46:32 -0800

    fully working with UI

--- QueenAttack/Models/BusinessLogic.cs
+++ QueenAttack/Models/BusinessLogic.cs
@@ -12,14 +12,14 @@ namespace QueenAttack.Models
       get { return _queenX; }
       set { _queenX = value; }
     }
-    
+
     private int _queenY;
     public int QueenY
     {
       get { return _queenY; }
       set { _queenY = value; }
     }
-    
+
     private int _targetX;
     public int TargetX
     {
@@ -33,7 +33,7 @@ namespace QueenAttack.Models
       get { return _targetY; }
       set { _targetY = value; }
     }
-    
+
     public Board(int coordinateX, int coordinateY, int targetCoordinateX, int targetCoordinateY)
     {
       _queenX = coordinateX;
@@ -41,72 +41,83 @@ namespace QueenAttack.Models
       _targetX = targetCoordinateX;
       _targetY = targetCoordinateY;
     }
-    
+
     public bool CaptureStraight()
     {
       if (QueenX == TargetX || QueenY == TargetY)
       {
         return true;
       }
-      else 
+      else
       {
         return false;
       }
     }
-    
+
     public Dictionary<int, Tuple<int, int>> QueenRein = new Dictionary<int, Tuple<int, int>>();
-    
+
     public void QueenDiagAdd()
     {
-      for (int i = 0; i < 8; i ++) // ++
+      for (int i = 0; i < 8; i++) // ++
       {
         int Qx = QueenX + i;
         int Qy = QueenY + i;
-        if (Qx == 9 || Qy == 9) 
+        if (Qx == 9 || Qy == 9)
         {
           break;
         }
         QueenRein.Add(i, new Tuple<int, int>(Qx, Qy));
       }
-      for (int i = 0; i < 8; i ++) // +-
+      for (int i = 0; i < 8; i++) // +-
       {
         int Qx = QueenX + i;
         int Qy = QueenY - i;
-        if (Qx == 9 || Qy == 0) 
+        if (Qx == 9 || Qy == 0)
         {
           break;
         }
-        QueenRein.Add(i+10, new Tuple<int, int>(Qx, Qy));
+        QueenRein.Add(i + 10, new Tuple<int, int>(Qx, Qy));
       }
-      for (int i = 0; i < 8; i ++)  // -+
+      for (int i = 0; i < 8; i++)  // -+
       {
         int Qx = QueenX - i;
         int Qy = QueenY + i;
-        if (Qx == 0 || Qy == 9) 
+        if (Qx == 0 || Qy == 9)
         {
           break;
         }
-        QueenRein.Add(i+20, new Tuple<int, int>(Qx, Qy));
+        QueenRein.Add(i + 20, new Tuple<int, int>(Qx, Qy));
       }
-      for (int i = 0; i < 8; i ++) // --
+      for (int i = 0; i < 8; i++) // --
       {
         int Qx = QueenX - i;
         int Qy = QueenY - i;
-        if (Qx == 0 || Qy == 0) 
+        if (Qx == 0 || Qy == 0)
         {
           break;
         }
-        QueenRein.Add(i+30, new Tuple<int, int>(Qx, Qy));
-      }    
+        QueenRein.Add(i + 30, new Tuple<int, int>(Qx, Qy));
+      }
     }
     public bool CaptureDiag()
     {
-    bool Capture = QueenRein.Values.Any(tuple => tuple.Item1 == TargetX && tuple.Item2 == TargetY);
+      bool Capture = QueenRein.Values.Any(tuple => tuple.Item1 == TargetX && tuple.Item2 == TargetY);
+      {
+        return Capture;
+      }
+    }
+    //change type
+    public bool EvaluateBoth(bool a, bool b)
     {
-      return Capture;
+      if (a == true || b == true)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
-    }
-    
     // newMethod() //need to rewrite this 
     // {
     //   public void InputQX()
--- QueenAttack/Program.cs
+++ QueenAttack/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using QueenAttack.Models;
+using System.Collections.Generic;
 
 namespace QueenAttack
 {
@@ -7,8 +8,30 @@ namespace QueenAttack
   {
     static void Main()
     {
-      Board testBoard = new Board(1, 2, 3, 4);
-      Console.WriteLine(testBoard);
+      Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+      Console.WriteLine("Enter Coordinates:");
+      Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+      Board newBoard = new Board(0,0,0,0);
+      Console.WriteLine("Queen X:");
+      string one = Console.ReadLine();
+      Console.WriteLine("Queen Y:");
+      string two = Console.ReadLine();
+      Console.WriteLine("Target X:");
+      string three = Console.ReadLine();
+      Console.WriteLine("Target Y:");
+      string four = Console.ReadLine();
+      newBoard.QueenX = int.Parse(one);
+      newBoard.QueenY = int.Parse(two);
+      newBoard.TargetX = int.Parse(three);
+      newBoard.TargetY = int.Parse(four);
+      newBoard.QueenDiagAdd();
+      Console.WriteLine(newBoard.EvaluateBoth(newBoard.CaptureDiag(), newBoard.CaptureStraight()));
+
     }
   }
 }
+
+// public bool CaptureDiag()
+// public void QueenDiagAdd()
+// public bool CaptureStraight()
+// public Board(int coordinateX, int coordinateY, int targetCoordinateX, int targetCoordinateY)
